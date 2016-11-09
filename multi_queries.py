0a6847182b13b7fe88c643aec8f1755640feffa9
Nabeel Saad <nsaad@google.com>
2016-11-09 15:04:29 +0000

    moving jobresults from run to completed and updated the info and outputting it

--- multi_queries.py
+++ multi_queries.py
@@ -29,6 +29,7 @@ import uuid
 import subprocess
 import sys
 import datetime
+import json
 
 from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials
@@ -60,7 +61,6 @@ def poll_job(bigquery, job):
 
 # [START loadCommands]
 def loadCommands(filename):
-    print("hello load")
     f = open(filename, 'r')
     
     for line in f:
@@ -82,7 +82,7 @@ def loadCommands(filename):
 # [START runCommands]
 def runCommands():
     for c in commands:
-        c.print_command_details()
+        #c.print_command_details()
         c.execute_x_times()
     #TODO add some logic to handle running simples more regularly
 
@@ -93,12 +93,16 @@ def main(project_id, commandsFile, batch, num_retries, interval):
     loadCommands(commandsFile)
     
     print("Time command running started: " + str(datetime.now()))
+    commands_start_time = int(round(time.time() * 1000))
     runCommands()
+    commands_end_time = int(round(time.time() * 1000))
     print("Time command running ended: " + str(datetime.now()))
+    print("Commands ran in " + str((commands_end_time - commands_start_time)))
     
     #TODO: poll all jobs and update their times
     
-    
+    poll_jobs_run()
+    output_completed_jobs();
     ''''
     #Creating a single command
     c = Command('simple', 12, '/opt/google-cloud-sdk/bin/bq query --nosync "SELECT COUNT(*) FROM publicdata:samples.wikipedia"')
@@ -109,16 +113,57 @@ def main(project_id, commandsFile, batch, num_retries, interval):
     print_jobs_run()   '''
     
 # [END run]
-   
+
+def output_completed_jobs():
+    for i in range(0, len(jobs_completed)):
+        jobs_completed[i].print_jobresult_details()
+
+# [START get_jobresult_with_id]
+def get_jobresult_with_id(job_id):
+    for i in range(0, len(jobs_run)):
+        if jobs_run[i].job_id == job_id:
+            return jobs_run[i]
+    return NONE
+
+# [END get_jobresult_with_id]   
      
-def print_jobs_run():
-    """Prints the dict contains the jobs run, their status and timings"""
+def poll_jobs_run():
+    """TODO Prints the dict contains the jobs run, their status and timings"""
+    '''
     for keys in jobs_run:
         print("jobId--> " + keys)
         for values in jobs_run[keys]:
             print (values,':',jobs_run[keys][values])
-        
+    '''
+    #while len(jobs_run) > 0:
+    #TODO: keep checking till this list is empty - how do we remove items from the list without messing up the iterator 
+    for item in jobs_run[:]: #i in range(0, len(jobs_run)):      
+        jb = item      
+        #TODO: This is currently BQ specific way of getting job_id
+        try:
+            output = subprocess.check_output(['bq', '--format', 'json', 'wait', jb.job_id, '1'])
+        except CalledProcessError as exc:
+            print("Status: FAIL", exc.returncode, exc.output)
+        else:
+            parsedjson = json.loads(output)
+            status = parsedjson['status']
+            state = status['state']
+            
+            if(state == "DONE"):
+                jb.status = "DONE"
+                
+                statistics = parsedjson['statistics']
+                jb.start_time = statistics['startTime']
+                jb.end_time = statistics['endTime']
+                jb.bytes_processed = statistics['totalBytesProcessed']
+                jb.duration = int(jb.end_time) - int(jb.start_time)
+                
+                jobs_completed.append(jb)
+                jobs_run.remove(jb)
 
+    print("jobs_run:" + str(len(jobs_run)))
+    print("jobs_completed: " + str(len(jobs_completed)))
+    
 #Class
 class Command:
     """The command object to be used for loading the queries to be run"""
@@ -143,15 +188,43 @@ class Command:
                 print("Status: FAIL", exc.returncode, exc.output)
             else:
                 #TODO: This is currently BQ specific way of getting job_id
-                print(output)
+                print(output) #Outputs the status of running the query
                 job_id_location = output.find(default_project_id) + len(default_project_id) + 1
+                job_id_newline = output.find("\n") 
+                job_id = output[job_id_location:job_id_newline]
+                jb = JobResult(job_id)
+                jobs_run.append(jb)
                 #print("job_id " + output[job_id_location:])
-                jobs_run[output[job_id_location:]] = {'status' : 'started', 'startTime' : '', 'endTime' : '', 'duration' : ''}
+                #jobs_run[output[job_id_location:]] = {'status' : 'started', 'startTime' : '', 'endTime' : '', 'duration' : ''}
+
+#Class
+class JobResult:
+    """The result details for each job run"""
+    job_id = 0
+    status = ""
+    start_time = ""
+    duration = ""
+    end_time = ""
+    bytes_processed = ""
+    
+    def __init__(self, job_id):
+        self.job_id = job_id
+    
+    def print_jobresult_job_id(self):
+        print('JobResult with job_id[' + self.job_id + ']')
+    
+    def print_jobresult_details(self):
+        print('JobResult with job_id[' + self.job_id + '] status[' + self.status + '] start_time[' + str(self.start_time) + 
+              '] end_time[' + str(self.end_time) + '] duration[' + str(self.duration) + '] bytes_processed[' + str(self.bytes_processed) + ']')    
 
 #Script defaults that can be set
 default_project_id="nsaad-demos"
+commands_start_time = 0
+commands_end_time = 0
 commands = [] #Used to store the commands loaded from the file
-jobs_run = {} #Used to store the IDs of all the jobs run and get their status and details
+jobs_run = [] #Used to store the job results of running jobs
+jobs_completed = [] #Used to store the job results of jobs that have been confirmed completed.
+#jobs_run = {} #Used to store the IDs of all the jobs run and get their status and details
 
 # [START main]
 if __name__ == '__main__':
